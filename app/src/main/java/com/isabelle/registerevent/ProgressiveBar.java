6ae8962b6008f3eda826329699a6a13e51f4dcc6
IsabelleFa <isaoliveira782@gmail.com>
2019-12-06 18:13:48 -0300

    inclusao de som

--- app/src/main/java/com/isabelle/registerevent/ProgressiveBar.java
+++ app/src/main/java/com/isabelle/registerevent/ProgressiveBar.java
@@ -3,6 +3,7 @@ package com.isabelle.registerevent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -23,12 +24,29 @@ public class ProgressiveBar extends AppCompatActivity {
 
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 2000;
-
+    MediaPlayer mp;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
+        mp = MediaPlayer.create(this, R.raw.s7_notification);
+
         super.onCreate(icicle);
         setContentView(R.layout.activity_progressive_bar);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    mp.start();
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent mainIntent = new Intent(ProgressiveBar.this, MainActivity.class);
+                ProgressiveBar.this.startActivity(mainIntent);
+                ProgressiveBar.this.finish();
+                finish();
+            }
+        }).start();
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
